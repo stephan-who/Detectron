817ea3ee713710b3aa3524017d945b9f8a884dfd
gongxianglin <xianglin@iscas.ac.cn>
2019-07-10 17:41:45 +0800

    finish mask_rcnn_heads.py

--- detectron/code_fork/mask_rcnn_heads.py
+++ detectron/code_fork/mask_rcnn_heads.py
@@ -48,5 +48,235 @@ def add_mask_rcnn_outputs(model, blob_in, dim):
         )
 
         if cfg.MRCNN.UPSAMPLE_RATIO > 1:
-            blob_out = m
+            blob_out = model.BilinearInterpolation(
+                'mask_fcn_logits','mask_fcn_logits_up', num_cls, num_cls,
+                cfg.MRCNN.UPSAMPLE_RATIO
+            )
 
+    if not  model.train:
+        blob_out = model.net.Sigmoid(blob_out, 'mask_fcn_probs')
+
+    return blob_out
+
+def add_mask_rncc_losses(model, blob_mask):
+    """Add Mask R-CNN specific losses"""
+    loss_mask = model.net.SigmoidCrossEntropyLoss(
+        [blob_mask, 'masks_init32'],
+        'loss_mask',
+        scale=model.GetLossScale() * cfg.MRCNN.WEIGHT_LOSS_MASK
+    )
+    loss_gradients = blob_utils.get_loss_gradients(model, [loss_mask])
+    model.AddLosses('loss_mask')
+    return loss_gradients
+
+# ---------------
+# Mask Heads
+# ----------------
+
+def mask_rcnn_fcn_head_v1up4convs(model, blob_in, dim_in, spatial_scale):
+    """v1up design: 4* (conv 3x3) ,convT 2x2."""
+    return mask_rcnn_fcn_head_v1upXconvs(
+        model, blob_in, dim_in, spatial_scale,4
+    )
+
+
+def mask_rcnn_fcn_head_v1upXconvs(model, blob_in, dim_in, spatial_scale, num_convs):
+    """v1upXconvs design: X * (conv 3x3), convT 2x2"""
+    current = model.RoIFeatureTransform(
+        blob_in,
+        blob_out='_[mask]_roi_feat',
+        blob_rois='mask_rois',
+        method=cfg.MRCNN.ROI_XFORM_METHOD,
+        resolution=cfg.MRCNN.ROI_XFORM_RESOLUTION,
+        sampling_ratio=cfg.MRCNN.ROI_XFORM_SAMPLING_RATIO,
+        spatial_scale=spatial_scale
+    )
+
+    dilation = cfg.MRCNN.DILATION
+    dim_inner = cfg.MRCNN.DIM_REDUCED
+
+    for i in range(num_convs):
+        current = model.Conv(
+            current,
+            '_[mask]_fcn' + str(i + 1),
+            dim_in,
+            dim_inner,
+            kernel =3,
+            dilation=dilation,
+            pad=1 * dilation,
+            stride=1,
+            weight_init=(cfg.MRCNN.CONV_INIT, {'std':0.001}),
+            bias_init=('ConstantFill',{'value':0.})
+        )
+
+    # upsample layer
+    model.ConvTranspose(
+        current,
+        'conv5_mask',
+        dim_inner,
+        dim_inner,
+        kernel=2,
+        pad=0,
+        stride=2,
+        weight_init=(cfg.MRCNN.CONV_INIT, {'std': 0.001}),
+        bias_init = const_fill(0.0)
+    )
+    blob_mask = model.Relu('conv5_mask', 'conv5_mask')
+
+    return blob_mask, dim_inner
+
+def mask_rcnn_fcn_head_v1up4convs_gn(model, blob_in, dim_in, spatial_scale):
+    """v1up design: 4*(conv 3x3), convT 2x2, with GroupNorm"""
+    return mask_rcnn_fcn_head_v1upXconvs_gn(
+        model, blob_in, dim_in, spatial_scale, 4
+    )
+
+
+def mask_rcnn_fcn_head_v1upXconvs_gn(model, blob_in, dim_in, spatial_scale, num_convs):
+    """v1upXconvs design: X * (conv 3x3), convT 2x2, with GroupNorm """
+    current = model.RoIFeatureTransform(
+        blob_in,
+        blob_out='_mask_roi_feat',
+        blob_rois='mask_rois',
+        method=cfg.MRCNN.ROI_XFORM_METHOD,
+        resolution=cfg.MRCNN.ROI_XFORM_RESOLUTION,
+        sampling_ratio=cfg.MRCNN.ROI_XFORM_SAMPLING_RATIO,
+        spatial_scale=spatial_scale
+    )
+    dilation = cfg.MRCNN.DILATION
+    dim_inner = cfg.MRCNN.DIM_REDUCED
+
+    for i in range(num_convs):
+        current = model.ConvGN(
+            current,
+            '_mask_fcn' + str(i + 1),
+            dim_in,
+            dim_inner,
+            group_gn=get_group_gn(dim_inner),
+            kernel=3,
+            pad=1 * dilation,
+            stride=1,
+            weight_init=(cfg.MRCNN.CONV_INIT, {'std': 0.001}),
+            bias_init=('ConstantFill', {'value': 0.})
+        )
+        current = model.Relu(current, current)
+        dim_in = dim_inner
+
+    # upsample layer
+    model.ConvTranspose(
+        current,
+        'conv5_mask',
+        dim_inner,
+        dim_inner,
+        kernel=2,
+        pad=0,
+        stride=2,
+        weight_init=(cfg.MRCNN.CONV_INIT, {'std': 0.001}),
+        bias_init=const_fill(0.0)
+    )
+    blob_mask = model.Relu('conv5_mask', 'conv5_mask')
+
+    return blob_mask, dim_inner
+
+def mask_rcnn_fcn_head_v1up(model, blob_in, dim_in, spatial_scale):
+    """v1up design: 2 * (conv 3x3), convT 2x2."""
+    return mask_rcnn_fcn_head_v1upXconvs(
+        model, blob_in, dim_in, spatial_scale, 2
+    )
+
+def mask_rcnn_fcn_head_v0upshare(model, blob_in, dim_in, spatial_scale):
+    """Use a ResNet "conv5" / "stage5" head for mask prediction. Weights and computation are
+    shared with the conv5 box head. Computation can only be shared during training, since
+    inference is cascaded.
+
+    v0upshare design: conv5, convT 2x2.
+    """
+    # Since box and mask head are shared , these must match
+    assert cfg.MRCNN.ROI_XFORM_RESOLUTION == cfg.FAST_RCNN.ROI_XFORM_RESOLUTION
+
+    if model.train:  # share computation with bbox head at training time
+        dim_conv5 = 2048
+        blob_conv5 = model.net.SampleAs(
+            ['res5_2_sum', 'roi_has_mask_int32'],
+            ['_[mask]_res5_2_sum_sliced']
+        )
+    else:  # re-compute at test time
+        blob_conv5, dim_conv5 = add_ResNet_roi_conv5_head_for_masks(
+            model,
+            blob_in,
+            dim_in,
+            spatial_scale
+        )
+
+    dim_reduced = cfg.MRCNN.DIM_REDUCED
+
+    blob_mask= model.ConvTranspose(
+        blob_conv5,
+        'conv5_mask',
+        dim_conv5,
+        dim_reduced,
+        kernel=2,
+        pad=0,
+        stride=2,
+        weight_init=(cfg.MRCNN.CONV_INIT, {'std': 0.001}),  # std only for
+        bias_init=const_fill(0.0)
+    )
+    model.Relu('conv5_mask', 'conv5_mask')
+
+    return blob_mask, dim_reduced
+
+def mask_rcnn_fcn_head_v0up(model, blob_in, dim_in, spatial_scale):
+    """v0up design:conv5 , deconv 2x2 (no weight sharing with the box head)."""
+    blob_conv5, dim_conv5 = add_ResNet_roi_conv5_head_for_masks(
+        model,
+        blob_in,
+        dim_in,
+        spatial_scale
+    )
+    dim_reduced = cfg.MRCNN.DIM_REDUCED
+
+    model.ConvTranspose(
+        blob_conv5,
+        'conv5_mask',
+        dim_conv5,
+        dim_reduced,
+        kernel=2,
+        pad=0,
+        stride=2,
+        weight_init=('GaussianFill', {'std': 0.001}),
+        bias_init = const_fill(0.0)
+    )
+
+    blob_mask = model.Relu('conv5_mask', 'conv5_mask')
+
+    return blob_mask, dim_reduced
+
+
+def add_ResNet_roi_conv5_head_for_masks(model, blob_in, dim_in, spatial_scale):
+    """Add a ResNet "conv5" / "stage5" head for predicting mask."""
+    model.RoIFeatureTransform(
+        blob_in,
+        blob_out='_[mask]_pools',
+        blob_rois='mask_rois',
+        method=cfg.MRCNN.ROI_XFORM_METHOD,
+        resolution=cfg.MRCNN.ROI_XFORM_RESOLUTION,
+        sampling_ratio=cfg.MRCNN.ROI_XFORM_SAMPLING_RATIO,
+        spatial_scale=spatial_scale
+    )
+
+    dilation = cfg.MRCNN.DILATION
+    stride_init= int(cfg.MRCNN.ROI_XFORM_RESOLUTION / 7)  # by default :2
+
+    s, dim_in = ResNet.add_stage(
+        model,
+        '_[mask]_res5',
+        '_[mask]_pool5',
+        3,
+        dim_in,
+        2048,
+        512,
+        dilation,
+        stride_init=stride_init
+    )
+
+    return s, 2048
